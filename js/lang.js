5e4d154e90e42faf379d287bf2a28b0561f362fc
zkelee25 <leejohn360@hotmail.com>
2025-11-05 09:32:02 +0800

    Delete js directory

--- js/lang.js
+++
@@ -1,5 +0,0 @@
-function switchLang(lang){
-    document.querySelectorAll('[data-en]').forEach(el=>{
-        el.textContent = (lang==='en') ? el.getAttribute('data-en') : el.getAttribute('data-zh');
-    });
-}
